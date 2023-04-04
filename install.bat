546c2749b28e43077490e034fec19a22821b2e4f
Nikolas Schaber <nikolas.schaber@opendoors.de>
2023-04-04 12:20:14 +0200

    initial commit

---
+++ install.bat
@@ -0,0 +1,33 @@
+@echo off
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+
+CALL tools/crawl_ffmpeg.bat
+CALL tools/setup_venv.bat
+CALL tools/update_pip.bat
+CALL tools/install_whisper.bat
+CALL tools/install_requirements.bat
+CALL tools/install_spacy.bat
